44bac6826c6fff90dae59ac945e6bcca24f786b0
RaulGF <raulgf1982@gmail.com>
2021-09-25 10:19:24 +0200

    upload 2

--- src/main/java/com/example/mi1web/otherController.java
+++ src/main/java/com/example/mi1web/otherController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/firstController")
-public class appController {
+//establecer ruta relativa en caso de conflictos
+//del @RequestMapping
+@RequestMapping("/secondController")
+public class otherController {
 
     @RequestMapping("/getFormulario")
     public String getFormulario(){
@@ -17,6 +19,7 @@ public class appController {
         return "formulario";
     }
 
+    //RR = necesario poner rutas relativas en html
     @RequestMapping("/procesaRespuesta")
     public String procesaFormulario(
             HttpServletRequest request,
@@ -25,12 +28,11 @@ public class appController {
     {
         //recibo info
         String recepcion = request.getParameter("campoFormulario");
-        //proceso info
-        String recepMoficado = recepcion + " King the world";
+        String recepMoficado = recepcion + " SECOND CONTROLLER";
         //paso a la vista
         model.addAttribute("viajero",recepMoficado);
         model.addAttribute("viajero2",edad);
 
         return "respuesta";
-     }
+    }
 }
